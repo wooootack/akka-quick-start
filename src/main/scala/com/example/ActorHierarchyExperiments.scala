6b7478f43454443e53b445e6f7b599da4f5abf09
Kyosuke <kyou0209@gmail.com>
2021-12-13 21:15:20 +0900

    開始停止のお試し

--- src/main/scala/com/example/ActorHierarchyExperiments.scala
+++ src/main/scala/com/example/ActorHierarchyExperiments.scala
@@ -1,10 +1,52 @@
 package com.example
 
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.Behavior
+import akka.actor.typed.{ActorSystem, Behavior, PostStop, Signal}
 import akka.actor.typed.scaladsl.AbstractBehavior
 import akka.actor.typed.scaladsl.ActorContext
 import akka.actor.typed.scaladsl.Behaviors
+
+object StartStopActor1 {
+  def apply(): Behavior[String] =
+    Behaviors.setup(context => new StartStopActor1(context))
+}
+
+class StartStopActor1(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  println("first started")
+  context.spawn(StartStopActor2(), "second")
+
+  override def onMessage(msg: String): Behavior[String] =
+    msg match {
+      case "stop" => Behaviors.stopped
+    }
+
+  override def onSignal: PartialFunction[Signal, Behavior[String]] = {
+    case PostStop =>
+      println("first stopped")
+      this
+  }
+
+}
+
+object StartStopActor2 {
+  def apply(): Behavior[String] =
+    Behaviors.setup(new StartStopActor2(_))
+}
+
+class StartStopActor2(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  println("second started")
+
+  override def onMessage(msg: String): Behavior[String] = {
+    // no messages handled by this actor
+    Behaviors.unhandled
+  }
+
+  override def onSignal: PartialFunction[Signal, Behavior[String]] = {
+    case PostStop =>
+      println("second stopped")
+      this
+  }
+
+}
 
 object PrintMyActorRefActor {
   def apply(): Behavior[String] =
@@ -40,12 +82,18 @@ class Main(context: ActorContext[String]) extends AbstractBehavior[String](conte
         val hogeRef = context.spawn(PrintMyActorRefActor(), "hogehoge-actor")
         hogeRef ! "printit"
         this
+      case "stop" =>
+        val first = context.spawn(StartStopActor1(), "first")
+        first ! "stop"
+        this
     }
 }
 
 object ActorHierarchyExperiments extends App {
   val testSystem = ActorSystem(Main(), "hogehogeSystem")
 
+
   testSystem ! "start"
   testSystem ! "hoge"
+  testSystem ! "stop"
 }
