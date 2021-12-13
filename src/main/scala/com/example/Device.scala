2b8aeb08c95da02f3b6f7c0edfe45a350d924446
Kyosuke <kyou0209@gmail.com>
2021-12-13 23:55:14 +0900

    test

---
+++ src/main/scala/com/example/Device.scala
@@ -0,0 +1,50 @@
+package com.example
+
+import akka.actor.typed.{ActorRef, Behavior, PostStop, Signal}
+import akka.actor.typed.scaladsl.AbstractBehavior
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.LoggerOps
+
+object Device {
+  def apply(groupId: String, deviceId: String): Behavior[Command] =
+    Behaviors.setup(context => new Device(context, groupId, deviceId))
+
+  // 多分これは抽象的なコマンドを定義したいだけと思う
+  // これがあることで、アクターが受け付けるメッセージの型をコマンドにできてるんじゃないかな
+  sealed trait Command
+
+  final case class ReadTemperature(requestId: Long, replyTo: ActorRef[RespondTemperature]) extends Command
+  final case class RespondTemperature(requestId: Long, value: Option[Double])
+
+  final case class RecordTemperature(requestId: Long, value: Double, replyTo: ActorRef[TemperatureRecorded]) extends Command
+  final case class TemperatureRecorded(requestId: Long)
+}
+
+class Device(context: ActorContext[Device.Command], groupId: String, deviceId: String)
+  extends AbstractBehavior[Device.Command](context) {
+  import Device._
+
+  var lastTemperatureReading: Option[Double] = None
+
+  context.log.info2("Device actor {}-{} started", groupId, deviceId)
+
+  override def onMessage(msg: Command): Behavior[Command] =
+    msg match {
+      case RecordTemperature(id, value, replyTo) =>
+        context.log.info2("Recorded temperature reading {} with {}", value, id)
+        lastTemperatureReading = Some(value)
+        replyTo ! TemperatureRecorded(id)
+        this
+
+      case ReadTemperature(id, replayTo) =>
+        replayTo ! RespondTemperature(id, lastTemperatureReading)
+        this
+    }
+
+  override def onSignal: PartialFunction[Signal, Behavior[Command]] = {
+    case PostStop =>
+      context.log.info2("Device actor {}-{} stopped", groupId, deviceId)
+      this
+  }
+}
